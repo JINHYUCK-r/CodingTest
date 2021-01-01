977b61ae7f8e0f9364be5bc1fb8e279f79c9f375
JINHYUCK-r <wlsgur7701@naver.com>
2021-01-01 15:23:00 +0900

    2839번

---
+++ src/com/codingTest/N2839.java
@@ -0,0 +1,29 @@
+package com.codingTest;
+
+import java.util.Scanner;
+
+//https://www.acmicpc.net/problem/2839
+//정확하게 N키로그램배달. 3키로 5키로이용  
+public class N2839 {
+
+	public static void main(String[] args) {
+		Scanner scan  = new Scanner(System.in);
+		int gram = scan.nextInt();
+		int cnt = 0;
+		
+		if(gram ==4 || gram == 7) {
+			cnt = -1;
+			cnt = (gram/5)+2;
+		}else if(gram%5 == 0) {
+			cnt = (gram/5);
+		}else if(gram%5 == 1 || gram%5 ==3) {
+			cnt = (gram/5)+1;
+		}else if(gram%5 == 2 || gram%5 ==4) {
+			cnt = (gram/5)+2;
+		
+		}
+		System.out.println(cnt);
+
+	}
+}
+
